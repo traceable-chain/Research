f7cf7a6a384f2ab09d33e4c275a19e89f7396c02
adelarja <adelarja@gmail.com>
2023-11-17 14:06:34 -0300

    Improved examle

--- mqtt/rust-mosquitto/src/main.rs
+++ mqtt/rust-mosquitto/src/main.rs
@@ -1,29 +1,22 @@
 use rumqtt::{MqttClient, MqttOptions, QoS};
-use tokio;
+use std::{thread, time::Duration};
 
-#[tokio::main]
-async fn main() {
-    // Define MQTT options
-    let mqtt_options = MqttOptions::new("my-client-id", "127.0.0.1", 1883)
-        .set_keep_alive(5)
-        .set_clean_session(true);
+fn main() {
+    let mqtt_options = MqttOptions::new("test-pubsub1", "localhost", 1883);
+    let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
 
-    // Create an MQTT client
-    let (mut client, _) = MqttClient::start(mqtt_options).unwrap();
+    mqtt_client.subscribe("bedroom/temperature", QoS::AtLeastOnce).unwrap();
+    let sleep_time = Duration::from_secs(1);
 
-    // Subscribe to a topic
-    client.subscribe("bedroom/temperature", QoS::AtMostOnce).unwrap();
+    thread::spawn(move || {
+        for i in 0..100 {
+            let payload = format!("publish {}", i);
+            thread::sleep(sleep_time);
+            mqtt_client.publish("bedroom/temperature", QoS::AtLeastOnce, false, payload).unwrap();
+        }
+    });
 
-    // Publish a message to a topic
-    client
-        .publish("bedroom/temperature", QoS::AtMostOnce, false, "2".as_bytes())
-        .unwrap();
-
-    // Wait for incoming messages (you might want to use a loop here)
-    // if let Some(message) = client.incoming().await.next().await {
-    //     println!("Received message: {:?}", message);
-    // }
-
-    // Disconnect from the broker when done
-    // client.disconnect().await.unwrap();
+    for notification in notifications {
+        println!("{:?}", notification)
+    }
 }
