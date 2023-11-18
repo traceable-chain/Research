cc79fcc9eca3fff87268403f3adac4a413a2b955
nhussein11 <nicolashussein14@gmail.com>
2023-11-18 15:09:19 -0300

    fix: adding match pattern to handle error when trying to connect to the mqtt client

--- mqtt/rust-mosquitto/src/main.rs
+++ mqtt/rust-mosquitto/src/main.rs
@@ -8,23 +8,29 @@ const MQTT_TOPIC: &str = "bedroom/temperature";
 
 fn main() {
     let mqtt_options = MqttOptions::new(MQTT_CLIENT_ID, MQTT_ADDRESS, MQTT_PORT);
-    let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
 
-    mqtt_client.subscribe(MQTT_TOPIC, QoS::AtLeastOnce).unwrap();
-    let sleep_time = Duration::from_secs(1);
+    // let (mut mqtt_client, notifications) = MqttClient::start(mqtt_options).unwrap();
+    match MqttClient::start(mqtt_options) {
+        Ok((mut mqtt_client, notifications)) => {
+            mqtt_client.subscribe(MQTT_TOPIC, QoS::AtLeastOnce).unwrap();
+            let sleep_time = Duration::from_secs(1);
 
+            thread::spawn(move || {
+                for i in 0..100 {
+                    let payload = format!("publish {}", i);
+
+                    thread::sleep(sleep_time);
+
+                    mqtt_client
                         .publish(MQTT_TOPIC, QoS::AtLeastOnce, false, payload)
-    thread::spawn(move || {
-        for i in 0..100 {
-            let payload = format!("publish {}", i);
-            thread::sleep(sleep_time);
-            mqtt_client
-                .publish("bedroom/temperature", QoS::AtLeastOnce, false, payload)
-                .unwrap();
-        }
-    });
+                        .unwrap();
+                }
+            });
 
-    for notification in notifications {
-        println!("{:?}", notification)
+            for notification in notifications {
+                println!("{:?}", notification)
+            }
+        }
+        Err(e) => println!("error: {:?}", e),
     }
 }
